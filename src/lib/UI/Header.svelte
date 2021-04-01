6655afaa787e8f72f5e4ba167ce583a796d51e78
Jason Gutierrez <jasong84@gmail.com>
2021-04-01 13:58:58 -0500

    Add link for github repo

--- src/lib/UI/Header.svelte
+++ src/lib/UI/Header.svelte
@@ -1,12 +1,17 @@
-<header
-    class="flex flex-col sm:flex-row items-center justify-between w-full
-    bg-red-600 px-8 py-4"
-  >
-    <h1 class="text-2xl text-white uppercase">
-      <a href="/">Svelte PokeDex</a>
-    </h1>
-    <p class="text-white">
-      A Svelte REST API demo using the
-      <a href="https://pokeapi.co/" class="text-white underline">PokéAPI</a>
-    </p>
-  </header>
+<script>
+  import Icon from 'svelte-awesome/components/Icon.svelte'
+  import { github } from 'svelte-awesome/icons'
+</script>
+
+<header class="flex flex-col sm:flex-row items-center justify-between w-full bg-red-600 px-8 py-4">
+  <h1 class="text-2xl text-white uppercase">
+    <a href="/">Svelte PokeDex</a>
+  </h1>
+  <p class="text-white">
+    A Svelte REST API demo using the
+    <a href="https://pokeapi.co/" class="text-white underline">PokéAPI</a>
+  </p>
+  <a href="https://github.com/cptcrunchy/shiny-poke-app" alt="link to github repository">
+    <Icon data={github} scale="2" />
+  </a>
+</header>
